c73db7a2642d7370a75e32a7fa1c2cbf39a268c3
Ramazan Haciyev <ramazanh@goldenpay.az>
2023-03-15 14:22:22 +0400

    movie page cutting

--- src/components/movie-slider/MovieSlider.jsx
+++ src/components/movie-slider/MovieSlider.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {NavLink} from "react-router-dom";
 import {FreeMode, Navigation, Pagination} from "swiper";
@@ -9,12 +9,12 @@ import Loader from "../loader/Loader";
 const key = '9f093681-2656-4e6f-ac06-6ee5ef514ff9';
 const API = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/'
 
-function MovieSlider({category, queryParams}) {
+function MovieSlider({category}) {
 
     const [movies, setMovie] = useState([]);
     const {name, identifier} = category;
 
-    const getMovies = () => {
+    const getMovies = useCallback(() => {
         fetch(`${API + identifier}?year=${'2023'}&month=MARCH`, {
             method: 'GET',
             headers: {
@@ -25,13 +25,13 @@ function MovieSlider({category, queryParams}) {
             .then(res => res.json())
             .then(json => setMovie(json.films || json.items))
             .catch(err => setMovie([]))
-    }
+    }, [identifier]);
 
     useEffect(() => {
         (async () => {
             await getMovies();
         })()
-    }, [])
+    }, [getMovies])
 
     return (
         <div style={{width: '100%', marginBottom: '80px'}}>
@@ -80,7 +80,10 @@ function MovieSlider({category, queryParams}) {
                     {movies.length > 0 ? movies.map(movie => (
                         <SwiperSlide key={movie?.kinopoiskId || movie?.filmId}>
                             <NavLink to={{pathname: '/movie', state: {movieId: movie?.kinopoiskId || movie?.filmId}}}>
-                                <img src={movie?.posterUrl} alt='poster'/>
+                               <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px'}}>
+                                   <img src={movie?.posterUrl} alt='poster'/>
+                                   <span className='movieTitle'>{movie?.nameRu || movie?.nameOriginal}</span>
+                               </div>
                             </NavLink>
                         </SwiperSlide>
                     )) : <Loader/>}
--- src/pages/movie-page/MoviePage.jsx
+++ src/pages/movie-page/MoviePage.jsx
@@ -1,14 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useLocation} from "react-router-dom";
 import styles from './styles.module.css';
+import Loader from "../../components/loader/Loader";
 
 function MoviePage() {
     const location = useLocation();
     const movieId = location.state.movieId;
     const [movie, setMovie] = useState({});
-    const [trailer, setTrailer] = useState([]);
+    const [trailer, setTrailer] = useState('');
 
-    const getTrailer = () => {
+    const youTubeTrailerExists = (url) => {
+        if (url.startsWith('https://youtu.be') || url.startsWith('https://youtube.com')) {
+            let shortUrl = url.lastIndexOf('/');
+            return 'https://youtube.com/embed/' + url.substring(shortUrl + 1);
+        }
+    }
+
+    const getTrailer = useCallback(() => {
         fetch(`${process.env.REACT_APP_API_URL}` + movieId + '/videos', {
             method: 'GET',
             headers: {
@@ -19,17 +27,16 @@ function MoviePage() {
             .then(res => res.json())
             .then(json => {
                 json.items.filter(trailer => {
-                    if (trailer.url.startsWith('https://youtu.be') || trailer.url.startsWith('https://youtube.com')) {
-                        let n = trailer.url.lastIndexOf('/');
-                        let embedLink = 'https://youtube.com/embed/' + trailer.url.substring(n + 1);
-                        setTrailer(embedLink);
+                    let embedUrl = youTubeTrailerExists(trailer.url);
+                    if (embedUrl) {
+                        setTrailer(embedUrl);
                     }
-                })
+                });
             })
-            .catch(err => setTrailer([]))
-    }
-    console.log('ggg', trailer)
-    const getMovieData = () => {
+            .catch(err => setTrailer(''))
+    }, [movieId]);
+
+    const getMovieData = useCallback(() => {
         fetch(`${process.env.REACT_APP_API_URL}` + movieId, {
             method: 'GET',
             headers: {
@@ -40,22 +47,36 @@ function MoviePage() {
             .then(res => res.json())
             .then(json => setMovie(json))
             .catch(err => setMovie({}))
-    }
+    }, [movieId])
+
 
     useEffect(() => {
         getMovieData();
         getTrailer();
-    }, [])
+    }, [getMovieData, getTrailer])
 
     return (
         <div style={{display: 'flex'}}>
             <div style={{width: '100%'}}>
                 <h1>{movie?.nameRu || movie?.nameOriginal}</h1>
-                <div className={`${styles.flex} ${styles.gap20} ${styles.alignCenter}`}>
+                <div className={`${styles.flex} ${styles.gap20}`} style={{marginTop: '30px'}}>
                     <img className={styles.posterImg} src={movie?.posterUrl} alt="poster"/>
                     <div className={styles.trailerContainer}>
-                        <p className={styles.description}>{movie?.description}</p>
-                        <iframe src={trailer} width="521" frameBorder={0} height="291"></iframe>
+
+                        <div style={{display: 'flex', flexDirection: 'column'}} className={styles.gap20}>
+                            <div  className={styles.flex} style={{gap: '10px'}}>
+                            {movie?.genres?.length > 0 ? movie?.genres.map(genre => (
+                                    <button key={genre?.genre} className={styles.badge}>{genre?.genre}</button>
+                            )) : null}
+                            </div>
+                            <p className={styles.description}>{movie?.description}</p>
+                        </div>
+                        <div style={{width: '521px', height: '320px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                            {trailer ?
+                                <iframe title='trailer' src={trailer} width="521" frameBorder={0} height="320"/> :
+                                <Loader style={{width: '521px'}}/>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
